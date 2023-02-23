1b8565a4aaa76569f9181a33360b0f824b15315f
Elijah Hood <eli@Elijahs-MacBook-Air.local>
2023-02-22 21:27:29 -0500

    updates

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -3,6 +3,10 @@ set -e
 
 # deal with remote
 echo "Checking if remote exists..."
+if ! git ls-remote heroku; then
+  echo "Adding heroku remote..."
+  git remote add heroku git@git.heroku.com/eli-auth-provider-router.git
+fi
 
 # push only origin/master to heroku/master - will do nothing if
 # master doesn't change.
