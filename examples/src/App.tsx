aafb2b92694b1d32a3352b228dfc56ca90b5b170
Elijah Hood <eli@Elijahs-MacBook-Air.local>
2023-02-25 20:29:22 -0500

    feat: add router types & add AuthPrivateRoute

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,5 +1,25 @@
 import React from "react";
 import { AuthProviderRouter } from "@core/auth-provider-router";
 export default function App() {
-  return <AuthProviderRouter />;
+  const ourRoutes = [
+    {
+      routeName: "/dashboard",
+      isProtected: true,
+      // element: <Component /> some component will go here, need to figure out how to accept props etc.
+    },
+    {
+      routeName: "/home",
+      isProtected: false,
+    },
+    {
+      routeName: "/home",
+      isProtected: true,
+    },
+  ];
+  return (
+    <>
+      <h1>Auth Provider Wrapper</h1>
+      <AuthProviderRouter routes={ourRoutes} />
+    </>
+  );
 }
--- packages/@core:auth-provider-router/src/AuthProviderRouter.tsx
+++ packages/@core:auth-provider-router/src/AuthProviderRouter.tsx
@@ -1,8 +1,52 @@
-import React, { useEffect } from "react";
+import React, { ReactElement } from "react";
+import { Route, Routes, Navigate, BrowserRouter } from "react-router-dom";
 
-export function AuthProviderRouter() {
-  useEffect(() => {
-    console.log("tesring");
-  }, []);
-  return <h1>Testing again </h1>;
+type RoutesMap = {
+  isProtected: boolean;
+  routeName: string;
+  element?: JSX.Element | ReactElement;
+};
+
+interface IAuthProviderRouter {
+  routes: RoutesMap[];
+  token?: string;
+}
+
+type AuthPrivateRoute = {
+  routeName?: string;
+  token?: string;
+  children: ReactElement;
+  isProtected: boolean;
+};
+
+export function AuthPrivateRoute({
+  children,
+  routeName,
+  token,
+  isProtected,
+}: AuthPrivateRoute): ReactElement {
+  if (!token || isProtected) {
+    <Navigate to={routeName} replace={true} />;
+  }
+  return children;
+}
+
+export function AuthProviderRouter({ routes, token }: IAuthProviderRouter) {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {routes.map(({ isProtected, routeName, element }, key) => (
+          <Route
+            key={key}
+            path={routeName}
+            element={
+              <AuthPrivateRoute isProtected={isProtected} token={token}>
+                {element}
+              </AuthPrivateRoute>
+            }
+          />
+        ))}
+      </Routes>
+    </BrowserRouter>
+  );
 }
