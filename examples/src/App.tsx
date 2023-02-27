cd2e69b88e6d3a34f92124d5550ee2f7a1668df4
Elijah Hood <eli@Elijahs-MacBook-Air.local>
2023-02-26 19:12:02 -0500

    update

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { AuthProviderRouter } from "@core/auth-provider-router";
+// import { About, Contact, Dashboard } from "./pages";
 export default function App() {
   const ourRoutes = [
     {
       routeName: "/dashboard",
       isProtected: true,
+      // element: <Dashboard />,
       // element: <Component /> some component will go here, need to figure out how to accept props etc.
     },
     {
-      routeName: "/home",
+      routeName: "/about",
       isProtected: false,
+      // element: <About />,
     },
     {
-      routeName: "/login",
+      routeName: "/contact",
       isProtected: false,
+      // element: <Contact />,
     },
   ];
 
@@ -23,7 +27,7 @@ export default function App() {
   };
   return (
     <>
-      <h1>Auth Provider Wrapper</h1>
+      <h1>Auth Providewr Wrapper</h1>
       <AuthProviderRouter
         routes={ourRoutes}
         validateToken={validateToken}
---
+++ examples/src/pages/index.ts
@@ -0,0 +1,3 @@
+export { Dashboard } from "./Dashboard";
+export { Contact } from "./Contact";
+export { About } from "./About";
